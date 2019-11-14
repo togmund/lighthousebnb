c68eac1d74d5e02d70e30e0dd8a11fa1679b691c
Ted Ogmundson <togmund@gmail.com>
2019-11-14 02:52:27 +0000

    added a draft of type definitions

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,40 +1,47 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS properties CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS guest_reviews CASCADE;
+DROP TABLE IF EXISTS property_reviews CASCADE;
+DROP TABLE IF EXISTS rates CASCADE;
+
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY NOT NULL,
-  name,
-  email,
-  password
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255)
 );
 
 CREATE TABLE properties (
   property_id SERIAL PRIMARY KEY NOT NULL,
-  title,
-  description,
-  thumbnamil_photo_url,
-  cover_photo_url,
-  street,
-  parking_spaces,
-  number_of_bathrooms,
-  number_of_bedrooms,
-  country,
-  city,
-  province,
-  post_code,
-  active,
+  title VARCHAR(255),
+  description TEXT,
+  thumbnamil_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
+  street SMALLINT,
+  parking_spaces SMALLINT,
+  number_of_bathrooms SMALLINT,
+  number_of_bedrooms SMALLINT,
+  country VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(255),
+  post_code VARCHAR(255),
+  active boolean,
   user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE reservations (
   reservation_id SERIAL PRIMARY KEY NOT NULL,
-  start_date,
-  end_date,
+  start_date DATE,
+  end_date DATE,
   property_id INTEGER REFERENCES properties(property_id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE guest_reviews (
   guest_review_id SERIAL PRIMARY KEY NOT NULL,
-  rating,
-  message,
+  rating SMALLINT,
+  message TEXT,
   guest_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
   owner_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(reservation_id) ON DELETE CASCADE
@@ -42,8 +49,8 @@ CREATE TABLE guest_reviews (
 
 CREATE TABLE property_reviews (
   preperty_review_id SERIAL PRIMARY KEY NOT NULL,
-  rating,
-  message,
+  rating SMALLINT,
+  message TEXT,
   guest_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(property_id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(reservation_id) ON DELETE CASCADE
@@ -51,8 +58,8 @@ CREATE TABLE property_reviews (
 
 CREATE TABLE rates (
   rate_id SERIAL PRIMARY KEY NOT NULL,
-  start_date,
-  end_date,
-  cost_per_night,
+  start_date DATE,
+  end_date DATE,
+  cost_per_night INTEGER,
   property_id INTEGER REFERENCES properties(property_id) ON DELETE CASCADE
 );
