917fe7b0d11dcdf8a1e4181bc572bc96e2684fcb
Ted Ogmundson <togmund@gmail.com>
2019-11-14 23:29:12 +0000

    added injection-safe queries to the database for getUser, getEmail && addUser functions, tested live

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,7 +1,20 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lighthousebnb'
+});
+
 /// Users
+
+// getUserWithEmail
+// Accepts an email address and will return a promise.
+// The promise should resolve with the user that has that email address, or null if that user does not exist.
 
 /**
  * Get a single user from the database given their email.
@@ -9,18 +22,17 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool.query(`
+  SELECT *
+  FROM users
+  WHERE email = $1;
+  `, [email])
+  .then(res => res.rows[0]);
 }
 exports.getUserWithEmail = getUserWithEmail;
+
+// getUserWithId
+// Will do the same as getUserWithEmail, but using the user's id instead of email.
 
 /**
  * Get a single user from the database given their id.
@@ -28,10 +40,20 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`
+  SELECT *
+  FROM users
+  WHERE id = $1;
+  `, [id])
+  .then(res => res.rows[0]);
 }
 exports.getUserWithId = getUserWithId;
 
+// addUser
+// Accepts a user object that will have a name, email, and hashed password property.
+// This function should insert the new user into the database.
+// It will return a promise that resolves with the new user object. This object should contain the user's id after it's been added to the database.
+// Add RETURNING *; to the end of an INSERT query to return the objects that were inserted. This is handy when you need the auto generated id of an object you've just added to the database.
 
 /**
  * Add a new user to the database.
@@ -39,10 +61,14 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool.query(`
+    INSERT INTO users (
+      name, email, password) 
+    VALUES (
+      $1, $2, $3)
+    RETURNING *;
+  `, [user.name, user.password, user.email])
+  .then(res => console.log(res.rows[0].id));
 }
 exports.addUser = addUser;
 
@@ -67,14 +93,13 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  return pool.query(`
+  SELECT * FROM properties
+  LIMIT $1
+  `, [limit])
+  .then(res => res.rows);
 }
 exports.getAllProperties = getAllProperties;
-
 
 /**
  * Add a property to the database
