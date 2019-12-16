2cd87e87c70c9818bd7bfb38279b4c235fbc4748
togmund <togmund@gmail.com>
2019-12-16 02:21:03 +0000

    added dynamic search query for city, owner id, price per night and max price per night

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,17 +1,16 @@
-const properties = require('./json/properties.json');
-const users = require('./json/users.json');
+const properties = require("./json/properties.json");
+const users = require("./json/users.json");
 
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'lighthousebnb'
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "lightbnb"
 });
 
 /// Users
-
 
 /**
  * Get a single user from the database given their email.
@@ -19,15 +18,18 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  return pool.query(`
+  return pool
+    .query(
+      `
   SELECT *
   FROM users
   WHERE email = $1;
-  `, [email])
-  .then(res => res.rows[0]);
-}
+  `,
+      [email]
+    )
+    .then(res => res.rows[0]);
+};
 exports.getUserWithEmail = getUserWithEmail;
-
 
 /**
  * Get a single user from the database given their id.
@@ -35,31 +37,38 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return pool.query(`
+  return pool
+    .query(
+      `
   SELECT *
   FROM users
   WHERE id = $1;
-  `, [id])
-  .then(res => res.rows[0]);
-}
+  `,
+      [id]
+    )
+    .then(res => res.rows[0]);
+};
 exports.getUserWithId = getUserWithId;
-
 
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  return pool.query(`
+const addUser = function(user) {
+  return pool
+    .query(
+      `
     INSERT INTO users (
       name, email, password) 
     VALUES (
       $1, $2, $3)
     RETURNING *;
-  `, [user.name, user.password, user.email])
-  .then(res => console.log(res.rows[0].id));
-}
+  `,
+      [user.name, user.password, user.email]
+    )
+    .then(res => console.log(res.rows[0].id));
+};
 exports.addUser = addUser;
 
 /// Reservations
@@ -70,7 +79,9 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return pool.query(`
+  return pool
+    .query(
+      `
       SELECT properties.*, reservations.*, avg(rating) as average_rating
       FROM reservations
         JOIN properties ON reservations.property_id = properties.id
@@ -80,9 +91,13 @@ const getAllReservations = function(guest_id, limit = 10) {
       GROUP BY properties.id, reservations.id
       ORDER BY reservations.start_date
       LIMIT $2;
-  `, [guest_id, limit])
-  .then(res => {return res.rows;});
-}
+  `,
+      [guest_id, limit]
+    )
+    .then(res => {
+      return res.rows;
+    });
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
@@ -94,12 +109,70 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
-  .then(res => res.rows);
-}
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  console.log("*********************************************");
+  console.log(options);
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city ILIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    if (!queryParams.length) {
+      queryString += "WHERE ";
+    }
+    if (queryParams.length) {
+      queryString += "AND ";
+    }
+    queryParams.push(`${options.owner_id}`);
+    queryString += `owner_id = $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night) {
+    if (!queryParams.length) {
+      queryString += "WHERE ";
+    }
+    if (queryParams.length) {
+      queryString += "AND ";
+    }
+    queryParams.push(`${options.minimum_price_per_night}`);
+    queryString += `cost_per_night >= $${queryParams.length} `;
+  }
+
+  if (options.maximum_price_per_night) {
+    if (!queryParams.length) {
+      queryString += "WHERE ";
+    }
+    if (queryParams.length) {
+      queryString += "AND ";
+    }
+    queryParams.push(`${options.maximum_price_per_night}`);
+    queryString += `cost_per_night <= $${queryParams.length} `;
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then(res => res.rows);
+};
 exports.getAllProperties = getAllProperties;
 
 /**
@@ -112,5 +185,5 @@ const addProperty = function(property) {
   property.id = propertyId;
   properties[propertyId] = property;
   return Promise.resolve(property);
-}
+};
 exports.addProperty = addProperty;
--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -5,7 +5,6 @@ DROP TABLE IF EXISTS property_reviews CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
@@ -13,6 +12,7 @@ CREATE TABLE users (
 
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
 
   title VARCHAR(255) NOT NULL,
   description TEXT,
@@ -29,27 +29,22 @@ CREATE TABLE properties (
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
 
-  active BOOLEAN NOT NULL DEFAULT TRUE,
-
-  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
-
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
-
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  rating SMALLINT NOT NULL DEFAULT 0,
-  message TEXT,
-
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
 );
